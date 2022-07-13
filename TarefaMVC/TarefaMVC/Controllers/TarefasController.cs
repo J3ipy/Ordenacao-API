06569a1360989beebcc4887e254b3490e3b71f25
João Pedro <pedrosant1905@gmail.com>
2022-07-13 17:59:13 -0300

    Mudança no HTML

---
+++ TarefaMVC/TarefaMVC/Controllers/TarefasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TarefaMVC.Models;
+
+namespace TarefaMVC.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private bdTarefasEntities db = new bdTarefasEntities();
+
+        // GET: Tarefas
+        public ActionResult Index()
+        {
+            var tarefa = db.Tarefa.Include(t => t.Pessoa);
+            return View(tarefa.ToList());
+        }
+
+        // GET: Tarefas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefa.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Create
+        public ActionResult Create()
+        {
+            ViewBag.PESID = new SelectList(db.Pessoa, "PESID", "NOME");
+            return View();
+        }
+
+        // POST: Tarefas/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TARID,TARDESCRICAO,TARDATA,TARFEITO,PESID")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tarefa.Add(tarefa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PESID = new SelectList(db.Pessoa, "PESID", "NOME", tarefa.PESID);
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefa.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PESID = new SelectList(db.Pessoa, "PESID", "NOME", tarefa.PESID);
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TARID,TARDESCRICAO,TARDATA,TARFEITO,PESID")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarefa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PESID = new SelectList(db.Pessoa, "PESID", "NOME", tarefa.PESID);
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefa.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tarefa tarefa = db.Tarefa.Find(id);
+            db.Tarefa.Remove(tarefa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
