df901afe6c017c3dd23a764ac6ac9b97063058c0
João Pedro <pedrosant1905@gmail.com>
2022-07-19 14:45:12 -0300

    Solução de alguns problemas no controller

--- TarefaMVC/TarefaMVC/Controllers/TarefaController.cs
+++ TarefaMVC/TarefaMVC/Controllers/TarefaController.cs
@@ -10,18 +10,18 @@ using TarefaMVC.Models;
 
 namespace TarefaMVC.Controllers
 {
-    public class TarefasController : Controller
+    public class TarefaController : Controller
     {
         private bdTarefasEntities db = new bdTarefasEntities();
 
-        // GET: Tarefas
+        // GET: Tarefa
         public ActionResult Index()
         {
             var tarefa = db.Tarefa.Include(t => t.Pessoa);
             return View(tarefa.ToList());
         }
 
-        // GET: Tarefas/Details/5
+        // GET: Tarefa/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -36,19 +36,19 @@ namespace TarefaMVC.Controllers
             return View(tarefa);
         }
 
-        // GET: Tarefas/Create
+        // GET: Tarefa/Create
         public ActionResult Create()
         {
             ViewBag.PESID = new SelectList(db.Pessoa, "PESID", "NOME");
             return View();
         }
 
-        // POST: Tarefas/Create
+        // POST: Tarefa/Create
         // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
         // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "TARID,TARDESCRICAO,TARDATA,TARFEITO,PESID,NOME")] Tarefa tarefa)
+        public ActionResult Create([Bind(Include = "TARID,TARDESCRICAO,TARDATA,TARFEITO,PESID")] Tarefa tarefa)
         {
             if (ModelState.IsValid)
             {
@@ -61,7 +61,7 @@ namespace TarefaMVC.Controllers
             return View(tarefa);
         }
 
-        // GET: Tarefas/Edit/5
+        // GET: Tarefa/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -77,12 +77,12 @@ namespace TarefaMVC.Controllers
             return View(tarefa);
         }
 
-        // POST: Tarefas/Edit/5
+        // POST: Tarefa/Edit/5
         // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
         // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "TARID,TARDESCRICAO,TARDATA,TARFEITO,PESID,NOME")] Tarefa tarefa)
+        public ActionResult Edit([Bind(Include = "TARID,TARDESCRICAO,TARDATA,TARFEITO,PESID")] Tarefa tarefa)
         {
             if (ModelState.IsValid)
             {
@@ -94,7 +94,7 @@ namespace TarefaMVC.Controllers
             return View(tarefa);
         }
 
-        // GET: Tarefas/Delete/5
+        // GET: Tarefa/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -109,7 +109,7 @@ namespace TarefaMVC.Controllers
             return View(tarefa);
         }
 
-        // POST: Tarefas/Delete/5
+        // POST: Tarefa/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
